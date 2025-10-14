a0f1420f372db8cb06c4ac38e842bb90ef1dcf08
Lovely9387 <lovelybaglari06@gmail.com>
2025-10-15 03:02:29 +0530

    LAB 1 Assignment has been added
    - Added Hello program, self introduction program.

---
+++ LAB 1/Intro.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    printf("Name : Lovely Baglari \n");
+    printf("Roll Number : MEB25041 \n");
+    printf("Department : Mechanical \n");
+    printf("University : Tezpur University \n");
+
+
+    return 0;
+}
