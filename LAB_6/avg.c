d4c190c21b516f3d3b6f454bf5bc725bf0c5385c
Lovely9387 <lovelybaglari06@gmail.com>
2025-11-24 21:39:02 +0530

    Lab 6 task has been added.
    - Added digit calculation program, sum and average calculation program.

---
+++ LAB_6/avg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, x, sum = 0;
+    float average;
+    printf("Enter any number : ");
+    scanf("%d", &n);
+    while (n<0){
+    printf("Invalid Number !!! Enter Again : ");
+    scanf("%d", &n);
+    }
+    for(i=1; i<=n; i++)
+    {
+        printf("Enter a number :");
+        scanf("%d", &x);
+        sum = sum + x;
+        average = sum/n ;
+    } printf("Sum = %d and Average = %.2f", sum,average);
+    return 0;
+
+}
